d212daf0a4fee16d114c835958721a369259bc53
Marcuswkds <72952948+Marcuswkds@users.noreply.github.com>
2021-04-30 22:38:02 -0500

    Question 1, 2 and 4

---
+++ Source Code/Autoencoder.py
@@ -0,0 +1,90 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+import matplotlib.pyplot as plt
+
+# Load dataset
+from keras.datasets import mnist, fashion_mnist
+import numpy as np
+
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+# this is the size of our encoded representations
+encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+# this is our input placeholder
+input_img = Input(shape=(784,))
+
+# "encoded" is the encoded representation of the input
+
+hidden_layer1 = Dense(128, activation='relu')(input_img)  # add hidden layer
+encoded = Dense(encoding_dim, activation='relu')(hidden_layer1)
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(encoded)
+
+# this model maps an input to its reconstruction
+autoencoder = Model(input_img, decoded)
+
+# Creating a model for encoder (predictions to display test images)
+encoder = Model(input_img, encoded)
+encoded_input = Input(shape=(encoding_dim,))
+
+# Create a decoder model layer as the last layer from the autoencoder
+decoder_layer = autoencoder.layers[-1]
+# Create the decoder model
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+history = autoencoder.fit(x_train, x_train,
+                          epochs=50,
+                          batch_size=256,
+                          shuffle=True,
+                          verbose=2,
+                          validation_data=(x_test, x_test))
+
+
+# Predictions.
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+# Plotting the images and the reconstructed images. our images and reconstructions
+n = 1 # Number of images displayed.
+plot1 = plt.figure(figsize=(16, 4))
+for i in range(n):
+# Displaying the original images.
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+# Displaying the reconstructed images.
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+print((history.history.keys()))
+
+# Plotting the Accuracy.
+plot2 = plt.figure(2)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Accuracy Model')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='lower right')
+
+# Plotting the Loss.
+plot3 = plt.figure(4)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Loss Model')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
